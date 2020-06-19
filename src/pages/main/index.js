874ca21917f53997cbcf2940f11308aba2b5a5f6
Leo <leonardo-g-o@hotmail.com>
2020-06-19 09:12:14 -0400

    duck pattern

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -3,7 +3,7 @@ import propTypes from "prop-types";
 
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import * as FavoriteActions from "../../store/actions/favorites";
+import { Creators as FavoriteActions} from "../../store/ducks/favorites";
 
 class Main extends Component {
   static propTypes = {
@@ -18,6 +18,7 @@ class Main extends Component {
           url: propTypes.string,
         })
       ),
+      error: propTypes.oneOfType([null, propTypes.string])
     }).isRequired,
   };
   state = {
@@ -43,6 +44,8 @@ class Main extends Component {
           <button type="submit">Adicionar </button>
 
           {this.props.favorites.loading && <span>Carregando ... </span>}
+          
+          {!!this.props.favorites.error && <span style={{color: '#F00'}}>{this.props.favorites.error}</span>}
         </form>
         <ul>
           {this.props.favorites.data.map((favorite) => (
